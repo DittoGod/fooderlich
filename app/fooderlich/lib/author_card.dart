d8ef78c7b0e8093265b89c2306445fd01564c3ea
DittoGod <dittogod1@gmail.com>
2021-10-14 08:45:07 +0200

    Composed the author card

--- app/fooderlich/lib/author_card.dart
+++ app/fooderlich/lib/author_card.dart
@@ -20,7 +20,29 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
-        children: [],
+        // TODO 3: add alignment
+        children: [
+          Row(
+            children: [
+              CircleImage(
+                imageProvider: imageProvider,
+                imageRadius: 28,
+              ),
+              const SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    authorName,
+                    style: FooderlichTheme.lightTextTheme.headline2,
+                  ),
+                  Text(title, style: FooderlichTheme.lightTextTheme.headline3),
+                ],
+              )
+            ],
+          ),
+          // TODO 2: add IconButton
+        ],
       ),
     );
   }
