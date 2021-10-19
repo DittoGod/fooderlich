8a1955144baafb7cbb4456998fa775eb752695ff
DittoGod <dittogod1@gmail.com>
2021-10-19 12:28:24 +0200

    Updated code for interactive widgets

--- app/fooderlich/lib/screens/explore_screen.dart
+++ app/fooderlich/lib/screens/explore_screen.dart
@@ -1,70 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
 import '../models/models.dart';
-import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatefulWidget {
-  const ExploreScreen({Key? key}) : super(key: key);
-
-  @override
-  State<ExploreScreen> createState() => _ExploreScreenState();
-}
-
-class _ExploreScreenState extends State<ExploreScreen> {
+class ExploreScreen extends StatelessWidget {
   final mockService = MockFooderlichService();
-  late ScrollController _controller;
 
-  @override
-  void initState() {
-    _controller = ScrollController();
-    _controller.addListener(_scrollListener);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _scrollListener() {
-    if (_controller.offset >= _controller.position.maxScrollExtent &&
-        !_controller.position.outOfRange) {
-      print('i am at the bottom!');
-    }
-    if (_controller.offset <= _controller.position.minScrollExtent &&
-        !_controller.position.outOfRange) {
-      print('i am at the top!');
-    }
-  }
+  ExploreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // 1
     return FutureBuilder(
-      // 2
       future: mockService.getExploreData(),
-      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
-          return ListView(
-            // 6
-            controller: _controller,
-            scrollDirection: Axis.vertical,
-            children: [
-              // 7
-              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
-              // 8
-              const SizedBox(height: 16),
-              // 9
-              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
-            ],
-          );
+          return ListView(scrollDirection: Axis.vertical, children: [
+            TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+            const SizedBox(height: 16),
+            FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? [])
+          ]);
         } else {
-          // 10
           return const Center(child: CircularProgressIndicator());
         }
       },
